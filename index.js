db39172ed99302182f70a82ff79405327cc679d9
Jonas Laurinavičius <jonaslaurinavicius1@gmail.com>
2024-05-25 23:45:27 +0300

    Added additional features and updated CSS

--- index.js
+++ index.js
@@ -11,18 +11,17 @@ function addTask(){
     error.innerHTML = "";
     var ul = document.getElementById("list");
     var li = document.createElement("li");
-    var image = document.createElement("img");
+    li.classList.add("task-text");
+    
+    var taskCheckbox = document.createElement("input");
+    taskCheckbox.classList.add("task-checkbox");
+    taskCheckbox.setAttribute("type", "checkbox")
+
     var deleteButton = document.createElement("button");
     deleteButton.setAttribute("id", "delete-button");
+    deleteButton.innerHTML = '<img src="trashcan.png" />';
 
-
-
-    //taskCheckbox = document.createElement("li");
-    //taskCheckbox.setAttribute("type", "checkbox")
-
-
-
-    deleteButton.innerHTML = '<img src="icons8-x-48.png" />';
+    li.appendChild(taskCheckbox);
     li.appendChild(document.createTextNode(text));
     li.appendChild(deleteButton);
     ul.appendChild(li);
@@ -30,10 +29,38 @@ function addTask(){
     deleteButton.addEventListener('click', function(event) {
         event.target.closest('li').remove();
     });
+
+    document.getElementById("task-text").value = "";
 }
 
-button = document.getElementById("add-task");
+function removeAllTasks(){
+    list = document.getElementById("list");
+    list.innerHTML = "";
+}
 
-button.onclick = function(){
+function removeCompletedTasks(){
+    var tasks = Array.from(document.getElementsByClassName("task-checkbox"));
+    for(var i = 0; i < tasks.length; i++){
+        if(tasks[i].checked){
+            var li = tasks[i].closest("li");
+            if(li){
+                li.remove();
+            }
+        }
+    }
+}
+
+addButton = document.getElementById("add-task");
+addButton.onclick = function(){
     addTask();
+}
+
+removeAllButton = document.getElementById("remove-all");
+removeAllButton.onclick = function(){
+    removeAllTasks();
+}
+
+removeCompleted = document.getElementById("remove-completed");
+removeCompleted.onclick = function(){
+    removeCompletedTasks();
 }
